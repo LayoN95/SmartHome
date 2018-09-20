08985e867adb4fe6dc791bd6bc4d75c821a45c2c
LayoN95 <mborowczyk95@gmail.com>
2018-09-20 19:29:39 +0200

    converter

--- servers/http.js
+++ servers/http.js
@@ -3,6 +3,7 @@ var express = require('express'),
     sensorRoutes = require('./../routes/sensors'),
     resources = require('./../resources/model'),
     cors = require('cors');
+    converter = require('./../middleware/converter')
 
 var app = express();
 
@@ -14,5 +15,7 @@ app.use('/pi/sensors', sensorRoutes);
 app.get('/pi', function(req, res){
     res.send('To jest webowe PI!')
 });
+
+app.use(converter);
 
 module.exports = app;
