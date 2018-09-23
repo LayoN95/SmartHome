03967eb2014a1c23edde35538644fd8d141425c3
LayoN95 <mborowczyk95@gmail.com>
2018-09-23 18:34:14 +0200

    leds

--- wot-server.js
+++ wot-server.js
@@ -2,11 +2,12 @@ var httpServer = require('./servers/http'),
 resources = require('./resources/model');
 
 //Internal Plugins
-var dhtPlugin = require('./plugins/DHT11SensorPlugin');
-var ledsPlugin = require('./plugins/ledsPlugin');
+var dhtPlugin = require('./plugins/DHT11SensorPlugin'),
+    ledsPlugin = require('./plugins/ledsPlugin');
 
 dhtPlugin.start({'simulate': true, 'frequency': 10000});
 ledsPlugin.start({'simulate': false, 'frequency': 10000});
+
 var server = httpServer.listen(resources.pi.port, function(){
     console.info('Twoje webowe Pi jest uruchomione i dziala na porcie %s', resources.pi.port);
 });
