e0fe00ce38ad7c3cae01150fb5a7d918c45fdf9f
LayoN95 <mborowczyk95@gmail.com>
2018-09-23 18:07:32 +0200

    dodano plugin diody

--- wot-server.js
+++ wot-server.js
@@ -3,9 +3,10 @@ resources = require('./resources/model');
 
 //Internal Plugins
 var dhtPlugin = require('./plugins/DHT11SensorPlugin');
+var ledsPlugin = require('./plugins/ledsPlugin');
 
 dhtPlugin.start({'simulate': true, 'frequency': 10000});
-
+ledsPlugin.start({'simulate': false});
 var server = httpServer.listen(resources.pi.port, function(){
     console.info('Twoje webowe Pi jest uruchomione i dziala na porcie %s', resources.pi.port);
 });
