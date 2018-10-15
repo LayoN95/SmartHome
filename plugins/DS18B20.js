a2945f7747870e5166aceb64baa1553f6a4867a5
LayoN95 <mborowczyk95@gmail.com>
2018-10-16 01:07:18 +0200

    DS18B20 Database

--- plugins/DS18B20.js
+++ plugins/DS18B20.js
@@ -1,9 +1,23 @@
 var resources = require('./../resources/model');
 
+var mysql = require('mysql');
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "SmartHome",
+  password: "raspberry",
+  database: "SmartHome"
+});
+
 var interval, sensor;
 var model = resources.pi.sensors.ds18b20;
 var pluginName = resources.pi.sensors.ds18b20.name;
 var localParam = {'simulate': false, 'frequency': 2000};
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!")
+});
+
 
 exports.start = function (params) {
 	localParams = params;
@@ -37,10 +51,23 @@ var ds18b20 = require('ds18b20');
     ds18b20.temperature('28-00000a38e7b7', function(err, val) {
     model.value = val;
     console.info("Current temperature is", val);
-    
+    sendDB();
+
 	setTimeout (function () {
 	connectHardware();
 	}, localParams.frequecny);
 }
 )};
 
+function sendDB() {
+  var dateTime = require('node-datetime');
+  var dt = dateTime.create();
+  var date = dt.format('Y-m-d H:M:S');
+console.log(date.toString());
+  var sql = ("INSERT INTO `DS18B20` (id, temperature, date) VALUES ('',"+model.value+",NOW())");
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+ 
+};
